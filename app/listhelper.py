d4c0d6d48d6e112412bc97fdb53613aed985cf4f
dfamilia33 <dfamilia33@gmail.com>
2019-05-26 10:30:40 -0400

    Updates python to use order by and limit within queries

--- app/listhelper.py
+++ app/listhelper.py
@@ -1,11 +1,6 @@
 
 import math
 
-def slice_list(mylist, num):
-	if(len(mylist) > (num * 25)):
-		return mylist[(num *25)-25:(num *25)]
-	else:
-		return mylist[(num*25)-25:]
 
 def page_ind( num, dblen):
 	mylist = list()
--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,7 @@ Created on Fri Dec 28 14:27:24 2018
 from app import app, db
 from flask import render_template, request
 from app.models import Post
-from app.listhelper import slice_list, page_ind
+from app.listhelper import page_ind
 
 
 
@@ -19,7 +19,33 @@ def main():
 def alpha(num):
 
 
-	alphalist = Post.query.all()
+	if num > 1:
+		alphalist = Post.query.order_by(Post.word).offset((num * 25)-25).limit(num*25).all()
+		# FLask SQL-Alchemy creates a relationship between a databse and a python program
+		# to query, update, delete and create from a table within the python language
+
+		"""
+			SQL Equivalent:
+
+			SELECT *
+			FROM POST
+			ORDER BY WORD
+			OFFSET (num * 25) - 25
+			LIMIT 25
+
+		"""
+	else:
+		alphalist = Post.query.order_by(Post.word).limit(25).all()
+
+		"""
+			SQL Equivalent
+
+			SELECT *
+			FROM POST
+			ORDER BY WORD
+			LIMIT 25
+
+		"""
 
 	if((num*25) - 25 >= len(alphalist) ):
 		return render_template("error.html")
@@ -29,19 +55,39 @@ def alpha(num):
 	for post in alphalist:
 		flags.append(post.countries.split())
 
-	alphalist.sort(key=lambda post: (post.word))
-
-
-	return render_template("alpha.html", sentence = "JeviDict", 
-		postlist = slice_list(alphalist, num),
+	return render_template("alpha.html", sentence = "JeviDict", postlist = alphalist,
 	 flaglist = flags, indlist = page_ind(num, len(alphalist)))
 
 
 @app.route('/ranked/<int:num>')
 def ranked(num):    
 
-	ranklist = Post.query.all()
-	
+	if num > 1:
+		ranklist = Post.query.order_by(Post.upvotes.desc()).offset((num * 25)-25).limit(num*25).all()
+
+		"""
+			SQL Equivalent:
+
+			SELECT *
+			FROM POST
+			ORDER BY Upvotes DESC
+			OFFSET (num * 25) - 25
+			LIMIT 25
+
+		"""
+	else:
+		ranklist = Post.query.order_by(Post.upvotes.desc()).limit(25).all()
+
+		"""
+			SQL Equivalent:
+
+			SELECT *
+			FROM POST
+			ORDER BY Upvotes DESC
+			LIMIT 25
+
+		"""
+
 	if((num*25) - 25 >= len(ranklist) ):
 		return render_template("error.html")
 	
@@ -51,15 +97,40 @@ def ranked(num):
 	for post in ranklist:
 		flags.append(post.countries.split())
 
-	ranklist.sort(key=lambda post: (post.upvotes), reverse = True)
+	
 
 	return render_template("ranked.html", sentence = "JeviDict",
-	 postlist = slice_list(ranklist, num), flaglist = flags)
+	 postlist = ranklist, flaglist = flags, indlist = page_ind(num, len(ranklist)))
 
 
 @app.route('/time/<int:num>')
 def time(num):
-	timelist = Post.query.all()
+	
+	if num > 1:
+		timelist = Post.query.order_by(Post.timestamp.desc()).offset((num * 25)-25).limit(num*25).all()
+
+		"""
+			SQL Equivalent:
+
+			SELECT *
+			FROM POST
+			ORDER BY Timestamp DESC
+			OFFSET (num * 25) - 25
+			LIMIT 25
+
+		"""
+	else:
+		timelist = Post.query.order_by(Post.timestamp.desc()).limit(25).all()
+
+		"""
+			SQL Equivalent:
+
+			SELECT *
+			FROM POST
+			ORDER BY Timestamp DESC
+			LIMIT 25
+
+		"""
 	
 	if((num*25) - 25 >= len(timelist) ):
 		return render_template("error.html")
@@ -69,9 +140,8 @@ def time(num):
 	for post in timelist:
 		flags.append(post.countries.split())
 
-	timelist.sort(key=lambda post: (post.timestamp))
 	return render_template("newest.html", sentence = "JeviDict",
-	 postlist = slice_list(timelist, num), flaglist = flags)
+	 postlist = timelist, flaglist = flags, indlist = page_ind(num, len(timelist)))
 
 
 
