aa672aea586dc81d65b884351575803c97083502
dfamilia33 <dfamilia33@gmail.com>
2019-05-26 13:48:05 -0400

    Migrations folder add

--- app/__init__.py
+++ app/__init__.py
@@ -7,11 +7,13 @@ Created on Thu Dec 27 13:49:31 2018
 from flask import Flask, session
 from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
 app.config.from_object(Config) #forms object so database can be accessed
 db = SQLAlchemy(app) #database file represented as a Python object
+migrate = Migrate(app, db)
 
 
 from app import routes, models
