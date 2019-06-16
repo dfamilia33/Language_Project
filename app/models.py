ed0e6dd407c7e2847ed1eb2ed68e43ba3dffa4de
dfamilia33 <dfamilia33@gmail.com>
2019-06-16 17:47:11 -0400

    Migration dilemna workaround resolved

--- app/models.py
+++ app/models.py
@@ -11,13 +11,14 @@ from app import db
 #represents a word to definition post
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    word = db.Column(db.String(64))#, index=True, unique=True)
+    word = db.Column(db.String(64))
     definition = db.Column(db.String(256))
-    #countries = db.Column(db.String(100))
     upvotes = db.Column(db.Integer)
     downvotes = db.Column(db.Integer)
     timestamp = db.Column(db.String(120))
     countries = db.relationship('Country', backref='Post', lazy=True)
+    approved = db.Column(db.Boolean)
+    sentence = db.Column(db.String(256))
 
     def __repr__(self):
         return '<User {}>'.format(self.word)  
