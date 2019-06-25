12277329af11287fbded6c79c6f79ab2477f0dac
dfamilia33 <dfamilia33@gmail.com>
2019-06-25 16:47:20 -0400

    Production database

--- app/config.py
+++ app/config.py
@@ -3,6 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     # finds app database in package else creates one
-    SQLALCHEMY_DATABASE_URI = os.environ.get('sqlite:///app.db') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('sqlite:///app.db') or \
+     #   'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = False
